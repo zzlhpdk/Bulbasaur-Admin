c72feb18da01bbbf3f3b8196236a2a1b30cf377a
huangyue <zzlhpdk@qq.com>
2025-01-22 17:48:00 +0800

    🐞 fix: 修改pinia和router初始化顺序

--- src/pulgins/pinia.ts
+++
@@ -1,9 +0,0 @@
-import { App } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersist from 'pinia-plugin-persist'
-
-export default {
-  install(app: App) {
-    app.use(createPinia().use(piniaPluginPersist))
-  },
-}
